3856d10a537fdd9c39f126c7e85d18de144612dd
Melissa Rossi <mrossi113@gmail.com>
2017-06-26 20:07:35 -0400

    Add some javascript for google sign-in

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 /**
  * Created for DiscApp.
  */
+
+// auth2 is initialized with gapi.auth2.init() and a user is signed in.
+
+if (auth2.isSignedIn.get()) {
+  var profile = auth2.currentUser.get().getBasicProfile();
+  console.log('ID: ' + profile.getId());
+  console.log('Full Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail());
+}
