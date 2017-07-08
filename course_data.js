35abb2e2467eca5ad52cdaac351954ea8bd6caed
Christopher Ruoti <cruoti@gmail.com>
2017-07-08 11:23:30 -0400

    use fake DB call to get course names

--- course_data.js
+++ course_data.js
@@ -2,16 +2,21 @@
  * Created by cruot on 7/8/2017.
  */
 
-var courses = ["wickham", "center springs"];
+var courses = ["Wickham", "Center Springs", "Vernon"];
 
-function getCourseNames() {
+function getCourseNamesFromDB() {
+    /*
+    TODO: Replace with DB calls
+    return list of course names like `var courses`
+     */
     return courses;
 }
 
 function createCourseComboBoxOptions() {
+    var selectableCourses = getCourseNamesFromDB();
     var combo = document.getElementById("combo");
-    for (var i = 0; i < courses.length; i++) {
-        var course = courses[i];
+    for (var i = 0; i < selectableCourses.length; i++) {
+        var course = selectableCourses[i];
         var option = document.createElement("option");
         option.text = course;
         option.value = course;
